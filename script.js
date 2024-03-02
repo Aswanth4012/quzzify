af2873cfaad1f6ca86d980e4d1f1bdddd0d8ddc7
Aswanth <aswanthachu3456@gmail.com>
2024-03-02 16:57:17 +0530

    base disign

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question:"which active player had most international goal?",
+        answer:[
+            {text:"Lionel Messi", correct:false},
+            {text:"Cristiano Ronaldo", correct:true},
+            {text:"Kiliyan Mbappe", correct:false},
+            {text:"Neymar Junior", correct:false},
+        ]
+    },
+    {
+        question:"Wich player had the most freekick goals?",
+        answer:[
+            {text:"Lionel Messi", correct:true},
+            {text:"Cristiano Ronaldo", correct:false},
+            {text:"Maradona", correct:false},
+            {text:"Kevin Dibruine", correct:false},
+        ]
+    }
+]
